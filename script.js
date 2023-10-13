d285d0de7b75d36af9a972dd33d7bcf9ae1fa87d
nathanhoehndorf <143524758+nathanhoehndorf@users.noreply.github.com>
2023-10-13 08:31:41 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function nathansTheme() {
+
+    document.body.style.backgroundColor = "purple";
+    document.body.style.color = "white";
+
+}
+
+function chasesTheme() {
+
+    document.body.style.backgroundColor = "blue";
+    document.body.style.color = "lime";
+
+}
+
+function resetTheme() {
+
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+
+}
+
+function numberGuesser() {
+
+    let num =  parseInt(Math.random() * 10) + 1;
+    let guess = prompt('Guess a Number between 1-10!');
+    if (guess == num) {
+
+        alert('Correct!');
+
+    } else {
+
+        alert('Incorrect!');
+
+    }
+
+}
+
+class 
